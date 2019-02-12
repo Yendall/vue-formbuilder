0476633349b329801e9676cf9e1cd54209b07ff7
Max Yendall <yendallmax@gmail.com>
2019-02-13 09:42:24 +1100

    Started creating new elements

---
+++ src/components/form_elements/FormElementHeading.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+      <!--<el-input-->
+        <!--:placeholder="currentField.isPlaceholderVisible ? currentField.placeholder : ''">-->
+      <!--</el-input>-->
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'Heading',
+    props: ['currentField']
+  }
+</script>
